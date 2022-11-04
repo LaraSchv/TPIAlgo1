e5e7a086e8c93eaf3a188d84969467631a6eda32
Leopq23 <94202648+Leopq23@users.noreply.github.com>
2022-11-03 21:52:41 -0300

    Update auxiliares.cpp

--- template-alumnos/auxiliares.cpp
+++ template-alumnos/auxiliares.cpp
@@ -11,3 +11,65 @@
 #include "auxiliares.h"
 
 using namespace std;
+
+bool posValida(tablero& t, pos p){
+    if (coordenadaValida(t, p.first) && coordenadaValida(t, p.second)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool coordenadaValida(tablero& t, int pi){
+    if (0 <= pi && pi < t.size()){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool perteneceAJugadas(jugadas j,pos p){
+    bool pertenece = false;
+
+    for (int i = 0; i < j.size(); ++i) {
+        if (p == j[i].first){
+            pertenece = true;
+        }
+    }
+    return pertenece;
+}
+
+bool perteneceABanderitas(banderitas b,pos p){
+    bool pertenece = false;
+
+    for (int i = 0; i < b.size(); ++i) {
+        if (p == b[i]){
+            pertenece = true;
+        }
+    }
+    return pertenece;
+}
+
+banderitas borrarBanderitas(banderitas& b,pos p){
+    for (int i = 0; i < b.size(); ++i) {
+        if (p == b[i])
+            b.erase(b.begin()+i);
+    }
+    return b;
+}
+
+bool hayMinas(tablero t,pos p){
+    bool minas = false;
+
+    for (int i = 0; i < t.size(); ++i) {
+        for (int j = 0; j < t.size(); ++j) {
+            if (i == p.first && j == p.second){
+                if (t[i][j]){
+                    minas = true;
+                }
+            }
+        }
+    }
+
+    return minas;
+}

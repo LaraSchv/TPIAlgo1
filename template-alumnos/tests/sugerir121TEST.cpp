52da517813643bab174c10cbf0f298b5525a4974
LaraSchv <49816203+LaraSchv@users.noreply.github.com>
2022-11-04 16:10:36 -0300

    deberia andar una vez que este hecha la funcion

--- template-alumnos/tests/sugerir121TEST.cpp
+++ template-alumnos/tests/sugerir121TEST.cpp
@@ -1,8 +1,34 @@
-// Tests para la función sugerir121.
-// Ver el archovo README.md para ayuda
+// Tests para la función minasAdyacentes.
+// Ver el archivo README.md para ayuda
 
 #include "../ejercicios.h"
 #include "../auxiliares.h"
+#include "../definiciones.h"
 #include "gtest/gtest.h"
 
 using namespace std;
+
+
+tablero t5 = {
+        { cMINA,  cVACIA,  cMINA, cVACIA, cVACIA },
+        { cVACIA, cVACIA, cVACIA, cVACIA,  cMINA },
+        { cVACIA, cVACIA,  cVACIA, cVACIA, cVACIA },
+        { cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cMINA,  cVACIA, cMINA,  cVACIA, cVACIA },
+};
+
+banderitas b8 = { {3,0} };
+
+pos pVacio = {0,0};
+
+jugadas jugadasPatron = {
+        jugada(pos(1, 0), 1), jugada(pos(1, 1), 2), jugada(pos(1, 2), 1),
+        jugada(pos(4, 1), 2)
+};
+
+
+TEST(sugerirAutomatico121, jugadaBuena){
+    pos pSugerida = pVacio;
+    bool hay = sugerirAutomatico121(t5, b8, jugadasPatron, pSugerida);
+    ASSERT_EQ(pSugerida, pos(0, 1));
+}

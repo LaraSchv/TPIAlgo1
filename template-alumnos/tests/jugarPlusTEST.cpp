a039efd03c64137fa5f31ad821ef8aa29ad86843
LaraSchv <49816203+LaraSchv@users.noreply.github.com>
2022-11-04 16:08:59 -0300

    agrego test

--- template-alumnos/tests/jugarPlusTEST.cpp
+++ template-alumnos/tests/jugarPlusTEST.cpp
@@ -1,8 +1,30 @@
-// Tests para la función jugarPlus.
-// Ver el archovo README.md para ayuda
+// Tests para la función minasAdyacentes.
+// Ver el archivo README.md para ayuda
 
 #include "../ejercicios.h"
 #include "../auxiliares.h"
+#include "../definiciones.h"
 #include "gtest/gtest.h"
 
 using namespace std;
+
+
+tablero t3 = {
+        { cMINA,  cMINA,  cVACIA, cVACIA, cVACIA },
+        { cVACIA, cVACIA, cVACIA, cMINA,  cVACIA },
+        { cVACIA, cMINA,  cVACIA, cVACIA, cVACIA },
+        { cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cMINA,  cVACIA, cMINA,  cVACIA, cVACIA },
+};
+
+banderitas b = { {3,0} };
+
+pos p = {0,2};
+
+jugadas jugadasVacias = {};
+
+
+TEST(jugarPlus, jugadaBuena){
+    jugarPlus(t3, b, p, jugadasVacias);
+    ASSERT_EQ(jugadasVacias.size(), 18);
+}
